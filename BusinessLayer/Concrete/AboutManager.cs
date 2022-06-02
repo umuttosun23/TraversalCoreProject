739bd096440f3580240b44edcaa5601f6bc400e0
Umut Tosun <tosun.umutt@gmail.com>
2022-06-02 18:02:56 +0300

    Service interfaceleri oluşturuldu.

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+        public About getById(int id)
+        {
+            throw new NotImplementedException();  
+        }
+
+        public void Tadd(About t)
+        {
+            _aboutDal.insert(t);
+        }
+
+        public void Tdelete(About t)
+        {
+            _aboutDal.delete(t);
+        }
+
+        public List<About> TgetList()
+        {
+            return _aboutDal.getList();
+        }
+
+        public void Tupdate(About t)
+        {
+            _aboutDal.update(t);
+        }
+    }
+}
