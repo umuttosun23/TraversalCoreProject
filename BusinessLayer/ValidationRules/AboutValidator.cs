5dbd1a06d2c4635cebc3da898774ab2ff402d6f2
Umut Tosun <tosun.umutt@gmail.com>
2022-06-02 18:09:51 +0300

    About sınıfı için ValidationRules oluşturuldu.

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Bu Alan Boş Geçilemez !");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Bu Alanı Boş Geçilemez !");
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Bu Alan Boş Geçilemez !");
+
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("Bu Alanın Karakter Sınırı 1500 !"); 
+        }
+    }
+}
