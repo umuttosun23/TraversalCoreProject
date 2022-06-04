51e8fca7d3cdd0918c302a1f6bf4288c04730bd2
Umut Tosun <tosun.umutt@gmail.com>
2022-06-04 19:52:35 +0300

    Login işlemleri tamamlandı.

---
+++ TraversalCoreProje/Models/UserRegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Adınızı Giriniz")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Soyadınızı Giriniz")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adınızı Giriniz")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Epostanızı Giriniz")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifrenizi Giriniz")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifre Tekrar alanını Giriniz")]
+        [Compare("Password",ErrorMessage ="Şifreler uyuşmuyor.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
