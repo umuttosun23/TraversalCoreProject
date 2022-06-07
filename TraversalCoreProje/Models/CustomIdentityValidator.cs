31547841a35a9a9f1cd956f9b4533b2332c87628
Umut Tosun <tosun.umutt@gmail.com>
2022-06-07 13:59:48 +0300

    Custom Identity Validator ile password hataları override edildi.

---
+++ TraversalCoreProje/Models/CustomIdentityValidator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError
+            {
+                Code= "PasswordTooShort",
+                Description=$"Parola en az {length} karakter olmalıdır."
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError
+            {
+                Code = "PasswordRequiresLower",
+                Description = $"Parola en az 1 küçük harf içermelidir ('a'.'z')."
+            };
+        }
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError
+            {
+                Code = "PasswordRequiresUpper",
+                Description = $"Parola en az 1 büyük harf içermelidir ('A'.'Z')."
+            };
+        }
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = $"Parola en az 1 tane sembol içermelidir."
+            };
+        }
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError
+            {
+                Code = "PasswordRequiresDigit",
+                Description = $"Parola en az 1 rakam içermelidir ('0'.'9')."
+            };
+        }
+    }
+}
